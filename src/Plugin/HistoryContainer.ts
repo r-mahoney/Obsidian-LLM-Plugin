1e4906c9e79f7595566d71135ff35f9e4379afe1
Ryan Mahoney <rsmahoney211@gmail.com>
2024-02-12 17:45:01 -0600

    changing styling so user can click on full div for history item and adding ability to edit hpw th eprompt is displayed to user

--- src/Plugin/HistoryContainer.ts
+++ src/Plugin/HistoryContainer.ts
@@ -17,44 +17,58 @@ export class HistoryContainer {
 		containerToShow: HTMLElement,
 		chat: ChatContainer
 	) {
+		const listenerMap = new Map()
+		function eventListener(index: number) {
+			chat.resetChat();
+			hideContainer(parentElement);
+			showContainer(containerToShow);
+			chat.setMessages(true);
+			const messages = chat.getMessages();
+			chat.generateIMLikeMessgaes(messages);
+			containerToShow.querySelector(".messages-div")?.scroll(0, 9999);
+			const titleDiv =
+				parentElement.parentNode?.querySelector(".title-div");
+			const buttons = titleDiv?.querySelectorAll(".title-buttons");
+			const settingsIndex = 0;
+			const newChatIndex = 2;
+			buttons![newChatIndex].id = "active-button";
+			buttons![settingsIndex].id = "";
+		}
+		const toggleContentEditable = (
+			element: HTMLElement,
+			toggle: boolean
+		) => {
+			element.setAttr("contenteditable", toggle);
+		};
+
 		history.map((historyItem: ChatHistoryItem, index: number) => {
 			const item = parentElement.createDiv();
+			const listener = eventListener.bind(item)
+			listenerMap.set(item, listener)
 			item.innerHTML = historyItem.prompt;
 			const buttonsDiv = item.createDiv();
 			buttonsDiv.addClass("history-buttons-div");
-			const setHistory = new ButtonComponent(buttonsDiv);
+			const editPrompt = new ButtonComponent(buttonsDiv);
+			const savePrompt = new ButtonComponent(buttonsDiv);
 			const deleteHistory = new ButtonComponent(buttonsDiv);
+			item.addEventListener("click", listener(index))
 
 			item.className = "setting-item";
+			item.setAttr("contenteditable", "false");
 			item.addClass("history-item");
-			setHistory.buttonEl.addClass("set-history-button", "mod-cta");
+			savePrompt.buttonEl.setAttr("style", "display: none");
+			editPrompt.buttonEl.addClass("edit-prompt-button");
+			savePrompt.buttonEl.addClass("save-prompt-button");
+			editPrompt.setIcon("pencil");
+			savePrompt.setIcon("save");
 			deleteHistory.buttonEl.addClass(
 				"delete-history-button",
 				"mod-warning"
 			);
-			setHistory.buttonEl.id = "set-history-button";
 			deleteHistory.buttonEl.id = "delete-history-button";
 
-			setHistory.onClick(() => {
-				chat.resetChat();
-				this.plugin.settings.historyIndex = index;
-				hideContainer(parentElement);
-				showContainer(containerToShow);
-				chat.setMessages(true);
-				const messages = chat.getMessages();
-				chat.generateIMLikeMessgaes(messages);
-				containerToShow.querySelector(".messages-div")?.scroll(0, 9999)
-				const titleDiv =
-					parentElement.parentNode?.querySelector(".title-div");
-				const buttons = titleDiv?.querySelectorAll(".title-buttons");
-				const settingsIndex = 0;
-				const newChatIndex = 2;
-				buttons![newChatIndex].id = "active-button";
-				buttons![settingsIndex].id = "";
-			});
-
 			deleteHistory.setIcon("trash");
-			deleteHistory.onClick(() => {
+			deleteHistory.onClick((e: MouseEvent) => {
 				this.resetHistory(parentElement);
 				let updatedHistory = this.plugin.settings.promptHistory.filter(
 					(item, idx) => idx !== index
@@ -69,6 +83,21 @@ export class HistoryContainer {
 					containerToShow,
 					chat
 				);
+			});
+
+			editPrompt.onClick((e: MouseEvent) => {
+				e.stopPropagation();
+				item.removeEventListener("click", listenerMap.get(this))
+				toggleContentEditable(item, true);
+				editPrompt.buttonEl.setAttr("style", "display: none");
+				savePrompt.buttonEl.setAttr("style", "display: inline-flex");
+			});
+
+			savePrompt.onClick((e: MouseEvent) => {
+				e.stopPropagation();
+				toggleContentEditable(item, false);
+				editPrompt.buttonEl.setAttr("style", "display: inline-flex");
+				savePrompt.buttonEl.setAttr("style", "display: none");
 			});
 		});
 	}
