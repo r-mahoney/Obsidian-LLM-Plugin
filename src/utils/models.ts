d5f6147b2945eb633cdcb0417ce49dcf6d3eebe8
Evan Harris <echarris@smcm.edu>
2024-09-17 10:25:08 -0400

    Add additional GPT4All models

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -18,6 +18,12 @@ export const openAIModels: Record<string, Model> = {
 
 export const models: Record<string, Model> = {
 	...openAIModels,
+	"ChatGPT-3.5 Turbo GPT4All based": {
+		model: "gpt4all-gpt-3.5-turbo.rmodel",
+		type: GPT4All,
+		endpoint: chat,
+		url: "/v1/chat/completions",
+	},
 	"Mistral OpenOrca": {
 		model: "mistral-7b-openorca.Q4_0.gguf",
 		type: GPT4All,
@@ -66,8 +72,14 @@ export const models: Record<string, Model> = {
 		endpoint: chat,
 		url: "/v1/chat/completions",
 	},
-	Hermes: {
+	"Hermes 13B": {
 		model: "nous-hermes-llama2-13b.Q4_0.gguf",
+		type: GPT4All,
+		endpoint: chat,
+		url: "/v1/chat/completions",
+	},
+	"Hermes 7B": {
+		model: "Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf",
 		type: GPT4All,
 		endpoint: chat,
 		url: "/v1/chat/completions",
@@ -126,7 +138,9 @@ export const modelNames: Record<string, string> = {
 	"orca-mini-3b-gguf2-q4_0.gguf": "Mini Orca (Small)",
 	"mpt-7b-chat-newbpe-q4_0.gguf": "MPT Chat",
 	"wizardlm-13b-v1.2.Q4_0.gguf": "Wizard v1.2",
-	"nous-hermes-llama2-13b.Q4_0.gguf": "Hermes",
+	"nous-hermes-llama2-13b.Q4_0.gguf": "Hermes 13B",
+	"Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf": "Hermes 7B",
+	"gpt4all-gpt-3.5-turbo.rmodel": "ChatGPT-3.5 Turbo GPT4All based",
 	"gpt4all-13b-snoozy-q4_0.gguf": "Snoozy",
 	"em_german_mistral_v01.Q4_0.gguf": "EM German Mistral",
 	"gpt-3.5-turbo": "ChatGPT-3.5 Turbo",
